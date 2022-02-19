484916343ab2cabb40ac53abb250bebbed1ee44e
Sarocha sriwong <sarocha_sri@truecorp.co.th>
2022-02-19 12:12:25 +0700

    add state

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,10 +1,13 @@
 import React, {useState} from 'react';
-
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
 
 const Card = (props) => {
   const {onAdd} = props;
   const initialState = {title: "", desc: ""}
   const [note, setNote] = useState(initialState);
+  const [isExpanded, setExpanded] = useState(false);
 
   const handleChange = (e) => {
       const {name, value} = e.target;
@@ -19,12 +22,35 @@ const Card = (props) => {
     e.preventDefault();
   }
 
- return (
+  const expand = () => {
+    setExpanded(true);
+  }
+
+  return (
     <div>
       <form className="create-note">
-        <input name="title" placeholder="Title..." value={note.title} onChange={handleChange}/>
-        <textarea  name="desc" placeholder="Take a note..." rows="3" value={note.desc} onChange={handleChange}/>
-        <button onClick={handleSubmit}>Add</button>
+        {isExpanded && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
+
+        <textarea
+          name="desc"
+          onClick={expand}
+          onChange={handleChange}
+          value={note.desc}
+          placeholder="Take a note..."
+          rows={isExpanded ? 3 : 1}
+        />
+        <Zoom in={isExpanded}>
+          <Fab onClick={handleSubmit}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
